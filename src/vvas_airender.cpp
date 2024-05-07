5de7c619d2baff9aab0de4968c02ed2baf525d99
Christos <xrdelakas@gmail.com>
2024-05-07 18:44:10 +0300

    update

--- src/vvas_airender.cpp
+++ src/vvas_airender.cpp
@@ -230,7 +230,7 @@ overlay_node_foreach (GNode * node, gpointer kpriv_ptr)
 		if (idx == 1) {
 			std::sprintf(new_label_string, "Dy = %d and Dx = %d", new_ymax-new_ymin, new_xmax-new_xmin);
 		} else if (idx == 0) {
-			std::sprintf(new_label_string, "Y - %d and X = %d", new_ymax-new_ymin, new_xmax-new_xmin);
+			std::sprintf(new_label_string, "Y = %d, %d and X = %d, %d", new_ymin, new_ymax-new_ymin, new_xmin, new_xmax-new_xmin);
 		} else {
 			new_label_string = label_string;
 		}
